083553e709f1fe55a998fd51c6c174011ae11471
Medam Mahesh <memahesh219@gmail.com>
2017-05-06 19:24:37 +0530

    Primes Below A Given Value N

---
+++ 2-3-PrimesBelowN.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define N 100
+
+int main(){
+	int Pri[N], i, j, count, p;
+	Pri[0]=2;
+	Pri[1]=3;
+	p = 2;
+	for(i=4;i<N;i++){
+		count = 0;
+		for(j=2;j<i;j++){
+			if(i%j==0){
+				count = 1;
+			}
+		}
+		if(count==0){
+			Pri[p] = i;
+			p++;
+		}
+	}
+	
+	printf("Prime Numbers below %d are :\n", N);
+	
+	for(i=0;i<p;i++){
+		printf("%d\n", Pri[i]);
+		
+	}
+}
