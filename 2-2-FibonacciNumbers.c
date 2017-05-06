6f7304cb9ccc738af031841c6ef404c5d0eee586
Medam Mahesh <memahesh219@gmail.com>
2017-05-06 17:04:33 +0530

    Fibonacci Numbers (Just change the value of N and variable type[in case you need it for very large numbers])

---
+++ 2-2-FibonacciNumbers.c
@@ -0,0 +1,14 @@
+/*Fibonacci Numbers*/
+#include <stdio.h>
+#define N 20
+int main(){
+	int Fib[N], i;
+	Fib[0]=0;
+	Fib[1]=1;
+	for(i=2;i<N;i++){
+		Fib[i]=Fib[i-1]+Fib[i-2];
+	}
+	for(i=0;i<N;i++){
+		printf("Fibonacci Number %d is %d\n", i+1, Fib[i]);
+	}
+}
