5e8e9e2853b4ee31e0688bc1f82eee9d99bfaef4
Medam Mahesh <memahesh219@gmail.com>
2017-05-06 17:37:34 +0530

    Course Rating System

---
+++ 2-2-CourseRatings.c
@@ -0,0 +1,21 @@
+/*Course Rating*/
+#include <stdio.h>
+#define N 10
+
+int main(){
+	int Ratings[6]={0,0,0,0,0,0}, i, temp;
+	printf("Rate how do you feel about the course on a scale of 1 to 5 only integers\n");
+	for (i=0;i<N;i++){
+		printf("Student %d : ", i+1);
+		scanf("%d", &temp);
+		if(temp<6 && temp>0){
+			Ratings[temp]++;
+		}else{
+			printf("Please rate it on a scale of 1 to 5 only integers\n");
+		}
+	}
+	printf("Data of Student Ratings :\n");
+	for (i=1;i<6;i++){
+		printf("Number of %d-Ratings : %d\n", i, Ratings[i]);
+	}
+}
